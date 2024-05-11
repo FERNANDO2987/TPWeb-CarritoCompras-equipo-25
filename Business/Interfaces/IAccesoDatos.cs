aabb5e5f336a61a49de030760db347ab7a358d68
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-10 23:00:57 -0300

    Add: se agrega nueva interfaz para Acceso a Datos

---
+++ Business/Interfaces/IAccesoDatos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+     public interface IAccesoDatos
+    {
+        SqlDataReader Lector { get; }
+        void setearConsulta(string consulta);
+        void ejecutarLectura();
+        void cerrarConexion();
+        void setearParametro(string nombreParametro, string valor);
+        void SetearParametroSeguro(string nombreParametro, string valor);
+
+    }
+}
