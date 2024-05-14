4138e2de74daa9596ae030a1b7b1c2aae1cb425a
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-13 23:00:19 -0300

    Add:se agreag nueva clase

---
+++ Business/Models/ListarArticulosEimagen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+     public class ListarArticulosEimagen
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public decimal Precio { get; set; }
+        public string DescripcionMarca { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenURL { get; set; }
+
+    }
+}
