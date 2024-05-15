8cb921e25b2cc12a8137e767b50ff73c633e6f5d
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-14 23:04:16 -0300

    Add: nueva clase

---
+++ Business/Models/DetalleProducto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class DetalleProducto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        
+        public int IdMarca { get; set; }
+        public string DescripcionMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public string DescripcionCategoria { get; set; }
+        public decimal Precio { get; set; }
+        public string ImagenURL { get; set; }
+    }
+}
