61cad1da6ecd6aa0aab710827f0092296dc9e5e1
Fernando2987 <fernandopalacios51@gmail.com>
2024-05-18 22:15:54 -0300

    Add:se agrega  clase para el articulo

---
+++ WebApp/Models/Articulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Articulo
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string ImagenURL { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
